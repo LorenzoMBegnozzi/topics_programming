f2922fe162c7071a4f1670885cdf2034dafe4e24
LorenzoBerg <lorenzobegnozzi@hotmail.com>
2025-03-02 22:49:04 -0300

    ping pong

--- ping-pong/script.js
+++ ping-pong/script.js
@@ -9,52 +9,107 @@ let xRaquete = 10;
 let yRaquete = 150;
 let larguraRaquete = 10;
 let alturaRaquete = 100;
+let velocidadeRaquete = 5;
 
-    function setup() {
-        createCanvas(600, 400);
+let xRaquete2 = 580;
+let yRaquete2 = 150;
+let velocidadeRaquete2 = 5;
 
-        vxBolinha = random(4, 10);
-        vyBolinha = random(4, 10);
+let pontosJogador1 = 0;
+let pontosJogador2 = 0;
 
-        if (random() > 0.5) vxBolinha *= -1;
-        if (random() > 0.5) vyBolinha *= -1;
-    }
+function setup() {
+    createCanvas(600, 400);
+    vxBolinha = random(4, 10);
+    vyBolinha = random(4, 10);
+
+    if (random() > 0.5) vxBolinha *= -1;
+    if (random() > 0.5) vyBolinha *= -1;
+}
 
 function draw() {
     background(0);
     criarBolinha();
-    vBolinha();
+    moverBolinha();
     validarColisaoBorda();
-    criarRaquete();
+    criarRaquete(xRaquete, yRaquete);
+    criarRaquete(xRaquete2, yRaquete2);
+    moverRaquete();
+    moverRaquete2();
+    validarColisaoRaquete(xRaquete, yRaquete);
+    validarColisaoRaquete(xRaquete2, yRaquete2);
+    exibirPlacar();
+}
 
-    function criarBolinha() {
-        circle(xBolinha, yBolinha, diametro);
+function criarBolinha() {
+    circle(xBolinha, yBolinha, diametro);
+}
+
+function moverBolinha() {
+    xBolinha += vxBolinha;
+    yBolinha += vyBolinha;
+}
+
+function validarColisaoBorda() {
+    if (xBolinha + raio > width) {
+        pontosJogador1++;
+        resetBolinha();
+    } else if (xBolinha - raio < 0) {
+        pontosJogador2++;
+        resetBolinha();
     }
 
-    function vBolinha() {
-        xBolinha += vxBolinha;
-        yBolinha += vyBolinha;
+    if (yBolinha + raio > height || yBolinha - raio < 0) {
+        vyBolinha *= -1;
     }
+}
 
-    function validarColisaoBorda() {
-        if (xBolinha + raio > width || xBolinha - raio < 0) {
-            vxBolinha *= -1;
-        }
+function criarRaquete(x, y) {
+    rect(x, y, larguraRaquete, alturaRaquete);
+}
 
-        if (yBolinha + raio > height || yBolinha - raio < 0) {
-            vyBolinha *= -1;
-        }
+function moverRaquete() {
+    if (keyIsDown(UP_ARROW)) {
+        yRaquete -= velocidadeRaquete;
     }
-
-    
-    function criarRaquete() {
-        rect(xRaquete, yRaquete, larguraRaquete, alturaRaquete);
+    if (keyIsDown(DOWN_ARROW)) {
+        yRaquete += velocidadeRaquete;
     }
+    yRaquete = constrain(yRaquete, 0, height - alturaRaquete);
+}
 
-    function validarColisaoRaquete() {
-        if (xBolinha - raio < xRaquete + larguraRaquete 
-            && yBolinha > yRaquete && yBolinha)
+function moverRaquete2() {
+    if (keyIsDown(87)) { // 'W'
+        yRaquete2 -= velocidadeRaquete2;
     }
+    if (keyIsDown(83)) { // 'S'
+        yRaquete2 += velocidadeRaquete2;
+    }
+    yRaquete2 = constrain(yRaquete2, 0, height - alturaRaquete);
+}
 
+function validarColisaoRaquete(x, y) {
+    if (
+        xBolinha - raio < x + larguraRaquete &&
+        xBolinha + raio > x &&
+        yBolinha > y &&
+        yBolinha < y + alturaRaquete
+    ) {
+        vxBolinha *= -1;
+    }
+}
 
+function resetBolinha() {
+    xBolinha = width / 2;
+    yBolinha = height / 2;
+    vxBolinha = random(4, 10) * (random() > 0.5 ? 1 : -1);
+    vyBolinha = random(4, 10) * (random() > 0.5 ? 1 : -1);
+}
+
+function exibirPlacar() {
+    fill(255);
+    textSize(20);
+    textAlign(CENTER, TOP);
+    text(pontosJogador1, width / 4, 20);
+    text(pontosJogador2, (3 * width) / 4, 20);
 }
